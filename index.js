bbdd9ba3e12016241729b278a67106d66f72da8a
Nowell Gata <nowell.gata@gmail.com>
2019-06-11 09:43:13 -0500

    Create .vscode directory

--- index.js
+++ index.js
@@ -12,30 +12,28 @@ const router = new Navigo(window.location.origin);
 const root = document.querySelector('#root');
 // render receives an argument as a named parameter: 'state'
 
-function render(state){
-    // TODO: Use Shadow DOM and Virtual DOM 'diffing' to avoid re-rendering ALL of the components
-    root.innerHTML = `
+function render(state) {
+	// TODO: Use Shadow DOM and Virtual DOM 'diffing' to avoid re-rendering ALL of the components
+	root.innerHTML = `
   ${Navigation(state)}
   ${Content(state)}
   ${Footer(state)}
   `;
 
-    router.updatePageLinks();
+	router.updatePageLinks();
 }
 
-function handleRoutes(params){}
+function handleRoutes(params) {}
 
 router
-    .on(':path', (params) => {
-        render(states[capitalize(params.path)]);
-    })
-    .on('/', () => render(states.Home))
-    .resolve();
-
+	.on(':path', params => {
+		render(states[capitalize(params.path)]);
+	})
+	.on('/', () => render(states.Home))
+	.resolve();
 
 // Fetch returns a Promise that contains a RESPONSE Object
 // Common structure when using FETCH
 fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-
+	.then(response => response.json())
+	.then(json => console.log(json));
